70dc794ed89376e7a42f7c44590317d2f2f80106
maharvey2 <mah0013mobile@gmail.com>
2019-07-07 13:45:26 -0500

    Starting over the the API part.

--- lib/spartanrace/api.rb
+++ lib/spartanrace/api.rb
@@ -1,17 +1,11 @@
-class SpartanRace::API
+#class SpartanRace::API
 # start of API pull with httparty
 #require 'httparty'
 #currently it is set to a zip, have to figure out how to change that 
+response = HTTParty.get('https://api2.spartan.com/api/races/search/?place=yes&limit=200&query=35401')
 
-def get_race
-response = HTTParty.get('https://api2.spartan.com/api/races/search/?place=yes&limit=200&query=35401', format: :plain)
-body = JSON.parse(response.body)
-id = body["id"]
-
-#puts response.body
+puts response.body
 #puts response.code
 #puts response.message
 #puts response.headers.inspect
 #races = response["location"]
-    end
-end
