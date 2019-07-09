2b5fd36cc9f51ab0894175b5879a7eb82a8b9d2f
maharvey2 <mah0013mobile@gmail.com>
2019-07-08 22:02:48 -0500

    restart on api

--- lib/spartanrace/api.rb
+++ lib/spartanrace/api.rb
@@ -1,11 +1,1 @@
-#class SpartanRace::API
-# start of API pull with httparty
-#require 'httparty'
-#currently it is set to a zip, have to figure out how to change that 
-response = HTTParty.get('https://api2.spartan.com/api/races/search/?place=yes&limit=200&query=35401')
-
-puts response.body
-#puts response.code
-#puts response.message
-#puts response.headers.inspect
-#races = response["location"]
+screw it reset this thing
--- lib/spartanrace/race.rb
+++ lib/spartanrace/race.rb
@@ -5,15 +5,43 @@ class SpartanRace::Race
     @@all = []
 
     def initialize(attrs)
-        set_race
+        set_int_id
         attrs_from_hash(attrs)
         save
     end
 
-    def set_race
+    def set_int_id
         @set_race = @@all.length + 1
     end
 
-    
+    def self.new_from_collection(races)
+        races.each do |attrs|
+            new(attrs)
+    end
+end
 
+    def attrs_from_hash(attrs)
+        attrs.each do |k, v|
+            send("#{k}=" , v)
+    end
+end
+
+    def self.get_races
+        SpartanRace::API.get_races
+        all
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.find_by_id(input)
+        all.find{|s| s.int_id == input.to_i}
+    end
+
+
+
+
+
+    #end for the class
 end
