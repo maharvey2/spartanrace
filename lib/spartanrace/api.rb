fd24009aa78056cb97d96f210157d4a0275a77de
maharvey2 <mah0013mobile@gmail.com>
2019-07-10 09:40:27 -0500

    making progress with understanding httparty

--- lib/spartanrace/api.rb
+++ lib/spartanrace/api.rb
@@ -1,1 +1,13 @@
-screw it reset this thing
+require 'rubygems'
+require 'httparty'
+
+class SpartanRace::API
+    include HTTParty
+    base_uri 'https://api2.spartan.com/api/races/search/'
+
+    response = HTTParty.get('https://api2.spartan.com/api/races/search/')
+
+    puts response.body
+
+
+end
