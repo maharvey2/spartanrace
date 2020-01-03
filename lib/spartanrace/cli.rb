1a126687e4abf26befb32c748eb74f7e805bb994
Mark Harvey <maharvey2@crimson.ua.edu>
2020-01-03 13:58:14 -0500

    added Case/When control

--- lib/spartanrace/cli.rb
+++ lib/spartanrace/cli.rb
@@ -1,18 +1,35 @@
 class SpartanRace::CLI
   def start
     puts "Welcome to SpartanRace Gem!\n"
-    user_input
+    #user_input
+    menu
   end
 
   def menu
+    puts "Would you to race?"
+    puts "yes or no?"
+
   input = ""
 
-  while input != "quit"
+  while input != "no"
+    
+    input = gets.strip
 
+    case input
+    when "yes"
+      user_input
+
+    when "again"
     list_races
-    get_option
-    race_data
-    start_over
+    when "no"
+      puts "not sure what to do"
+      exit
+    end
+  end
+    #list_races
+    #get_option
+    #race_data
+    #start_over
   end
 
   def user_input
@@ -21,7 +38,7 @@ class SpartanRace::CLI
     zip = valid_input(input)
     api = SpartanRace::Scraper.new(zip)
     api.get_races
-    menu
+    list_races
   end
 
   def valid_input(input)
@@ -33,14 +50,17 @@ class SpartanRace::CLI
   end
 
   def list_races
+    input = @option
     puts "\nHere are nearby races!"
     SpartanRace::Race.all.each { |sr| puts "#{sr.int_id}. #{sr.name}. #{sr.state}." }
+    get_option
   end
 
   def get_option
     puts "\nWould you like to know more?"
     puts "Select a Race by number or 'quit' to leave\n "
     @option = gets.strip.to_i
+    race_data
   end
 
   def race_data
@@ -50,6 +70,7 @@ class SpartanRace::CLI
     puts "#{individual_race.state}"
     puts "#{individual_race.start_date}"
     puts "#{individual_race.venue_description.gsub!(/<\/?[^>]*>/, "")}"
+    puts "try again? (again)"
   end
 
   def start_over
